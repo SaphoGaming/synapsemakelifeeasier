f77aa3c4c2984c4465c7eda96a0ae137e9d64bc1
SaphoGaming <49611780+SaphoGaming@users.noreply.github.com>
2022-08-20 15:48:22 +0300

    s
    dada

--- SynapseMakeLifeEasier.plugin.js
+++ SynapseMakeLifeEasier.plugin.js
@@ -284,7 +284,7 @@
    module.exports = class ExamplePlugin { 
       
       load() {
-          ZLibrary.PluginUpdater.checkForUpdate("SynapseMakeLifeEasier", "3.3.0", "https://raw.githubusercontent.com/SaphoGaming/synapsemakelifeeasier/main/SynapseMakeLifeEasier.plugin.js")
+          ZLibrary.PluginUpdater.checkForUpdate("SynapseMakeLifeEasier", "3.3.0", "https://raw.githubusercontent.com/SaphoGaming/synapsemakelifeeasier/main/SynapseMakeLifeEasier.plugin.js?token=GHSAT0AAAAAABX3BTG2ODKGBBDEOO7OZ7UQYYA3A5A")
       }
   
       start() {
